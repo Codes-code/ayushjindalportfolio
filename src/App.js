f3a2df4124bb0b4013c374a81843c25198cf90df
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-06-12 20:26:56 +0530

    projects mentioned.

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ function App() {
           Ayush Jindal | Portfolio Coming Soon...
         </h1>
 
-      <div>
-      <h3 className="ui header">some meme for time being</h3><br/><br/>
-      <img alt="edgy meme 1" src="./meme1.png"></img>
-      <img alt="edgy meme 2" src="./meme2.jpg"></img>
-      <img alt="edgy meme 3" src="./meme3.png"></img>
-      <img alt="edgy meme 4" src="./meme4.png"></img>
-      <img alt="edgy meme 5" src="./meme5.png"></img>
-      </div>
-
-      <div className="projects" style={{marginTop:'100px', marginBottom:'100px'}}>
-        <h2>Have a look at my React Projects</h2>
-        <div>
-          <a href="https://fireauth-8c520.web.app/" target="_blank">1. Authentication Project</a>
+        <div className="projects" style={{marginTop:'100px', marginBottom:'100px'}}>
+          <h2>Have a look at my React Projects</h2>
+          <div>
+            <a href="https://fireauth-8c520.web.app/" target="_blank">1. FireAuth | Authentication Project</a>
+          </div>
+          <div>
+            <a href="https://finderscopeproject.web.app/" target="_blank">2. FinderScope | Image Search</a>
+          </div>
         </div>
-      </div>
+
+        <div>
+          <h3 className="ui header">some meme for time being</h3><br/><br/>
+          <img alt="edgy meme 1" src="./meme1.png"></img>
+          <img alt="edgy meme 2" src="./meme2.jpg"></img>
+          <img alt="edgy meme 3" src="./meme3.png"></img>
+          <img alt="edgy meme 4" src="./meme4.png"></img>
+          <img alt="edgy meme 5" src="./meme5.png"></img>
+        </div>
 
       </div>
     </div>
