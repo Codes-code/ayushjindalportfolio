76b2e493925d7966a0782c7c18e8ec7baaa4775f
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-06-12 19:33:11 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className='ui header' style={{marginTop:'33px'}}>
+          Ayush Jindal | Portfolio Coming Soon...
+        </h1>
+
+      <div>
+      <h3 className="ui header">some meme for time being</h3><br/><br/>
+      <img alt="edgy meme 1" src="./meme1.png"></img>
+      <img alt="edgy meme 2" src="./meme2.jpg"></img>
+      <img alt="edgy meme 3" src="./meme3.png"></img>
+      <img alt="edgy meme 4" src="./meme4.png"></img>
+      <img alt="edgy meme 5" src="./meme5.png"></img>
+      </div>
+
+      <div className="projects" style={{marginTop:'100px', marginBottom:'100px'}}>
+        <h2>Have a look at my React Projects</h2>
+        <div>
+          <a href="https://fireauth-8c520.web.app/" target="_blank">1. Authentication Project</a>
+        </div>
+      </div>
+
+      </div>
     </div>
   );
 }
